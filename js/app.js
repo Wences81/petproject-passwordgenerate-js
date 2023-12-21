de056d51ba53c0894e35895439d1092162142bba
Wences81 <zhylinpochta@gmail.com>
2023-12-21 16:43:16 +0100

    21/12/23

--- js/app.js
+++ js/app.js
@@ -21,10 +21,26 @@ const generateBtn = document.createElement('button');
 generateBtn.classList.add('password-button');
 generateBtn.innerText = 'Generate password';
 generateBtn.addEventListener('click', (e) => {
-
+    let password = generatePassword(12);
+    passwordEl.value = password;
 });
 
+function generatePassword(passwordLength) {
+    const numberChars = '0123456789';
+    const upperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerChars = 'abcdefghijklmnopqrstuvwxyz';
+    const symbolChars = '!@#$%^&*()_+';
+    const allChars = numberChars + upperChars + lowerChars + symbolChars;
 
+    let randomString = '';
+
+    for (let i = 0; i < passwordLength; i++) {
+        let randomNumber = Math.floor(Math.random() * allChars.length);
+        randomString += allChars[randomNumber]
+    }
+
+    return randomString;
+}
 
 mainEl.appendChild(passwordEl);
 mainEl.appendChild(copyBtn);
